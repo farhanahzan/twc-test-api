93b8c75ce73aa37b31c384bedceb34ed7ebc81bd
farhanahzan <41515998+farhanahzan@users.noreply.github.com>
2023-02-13 14:42:45 +0530

    change none to none

--- Controllers/AuthControllers.js
+++ Controllers/AuthControllers.js
@@ -42,7 +42,7 @@ module.exports.register = async (req, res, next) => {
       withCredentials: true,
       httpOnly: false,
       maxAge: maxAge * 1000,
-      sameSite: 'none',
+      sameSite: 'None',
       secure: true,
     });
     res.status(201).json({ user: user._id, created: true });
